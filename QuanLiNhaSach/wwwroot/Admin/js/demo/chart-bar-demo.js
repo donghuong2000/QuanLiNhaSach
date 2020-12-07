83a39f1f1b39517fc4e0d83196e3b54b194660d2
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-12-07 07:25:24 +0700

    update

--- QuanLiNhaSach/wwwroot/Admin/js/demo/chart-bar-demo.js
+++ QuanLiNhaSach/wwwroot/Admin/js/demo/chart-bar-demo.js
@@ -68,7 +68,7 @@ var myBarChart = new Chart(ctx, {
             yAxes: [{
                 ticks: {
                     min: 0,
-                    max: 15000,
+                    
                     maxTicksLimit: 5,
                     padding: 10,
                     // Include a dollar sign in the ticks
