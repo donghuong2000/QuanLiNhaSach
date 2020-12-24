1844d56e66619d6854ce6bc74312dd1fa3439522
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-12-24 11:44:07 +0700

    update quy định

--- QuanLiNhaSach/Areas/Admin/Controllers/RuleController.cs
+++ QuanLiNhaSach/Areas/Admin/Controllers/RuleController.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuanLiNhaSach.Data;
+using QuanLiNhaSach.Models;
 
 namespace QuanLiNhaSach.Areas.Admin.Controllers
 {
+    [Area("admin")]
     public class RuleController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public RuleController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var rulelist = _db.Rules.ToList();
+            return View(rulelist);
+
         }
+        [HttpPost]
+        public IActionResult Update(string id,bool utr,int min,int max)
+        {
+            // utr = use this rule
+            // nin = nhập ít nhất
+            // tin = tồn ít nhất
+            try
+            {
+                var rule = _db.Rules.Find(id);
+                rule.UseThisRule = utr;
+                rule.Min = min;
+                rule.Max = max;
+
+                _db.Update(rule);
+                _db.SaveChanges();
+                return Json(new { success = true, message = "đã cập nhập thành công quy định này" });
+
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message =e.Message });
+            }
+        }
+
+        
+
     }
 }
---
+++ QuanLiNhaSach/wwwroot/Admin/js/rules.js
@@ -0,0 +1,78 @@
+﻿
+
+
+
+
+$('#updateqd-1').click(function () {
+    var useThisRule = $('#usethisrule1')[0].checked;
+    // nhập ít nhất
+    var nin = $('#min-1').val()
+
+    // tồn ít nhất
+    var tin = $('#max-1').val()
+
+
+    if (nin < 0 || tin < 0) {
+        toastr.error("Giá trị nhập vào phải lớn hơn 0")
+        return false
+    }
+        
+    console.log(useThisRule);
+    console.log(nin);
+    console.log(tin);
+    $.ajax({
+        url:'/Admin/rule/update',
+        method: 'post',
+        data: {id:'QD1', utr: useThisRule, min: nin, max: tin },
+        success: function (data) {
+            if (data.success)
+                toastr.success(data.message)
+            else
+                toastr.error(data.message)
+        }
+    })
+})
+$('#updateqd-2').click(function () {
+    var useThisRule = $('#usethisrule2')[0].checked;
+    // nhập ít nhất
+    var nin = $('#min-2').val()
+    // tồn ít nhất
+    var tin = $('#max-2').val()
+
+    if (nin < 0 || tin < 0) {
+        toastr.error("Giá trị nhập vào phải lớn hơn 0")
+        return false
+    }
+    console.log(useThisRule);
+    console.log(nin);
+    console.log(tin);
+    $.ajax({
+        url: '/Admin/rule/update',
+        method: 'post',
+        data: { id: 'QD2', utr: useThisRule, min: nin, max: tin },
+        success: function (data) {
+            if (data.success)
+                toastr.success(data.message)
+            else
+                toastr.error(data.message)
+        }
+    })
+})
+$('#updateqd-3').click(function () {
+    var useThisRule = $('#usethisrule3')[0].checked;
+    // nhập ít nhất
+    var nin = $('#min-1').val()
+    // tồn ít nhất
+    var tin = $('#max-1').val()
+    $.ajax({
+        url: '/Admin/rule/update',
+        method: 'post',
+        data: { id: 'QD3', utr: useThisRule, min: nin, max: tin },
+        success: function (data) {
+            if (data.success)
+                toastr.success(data.message)
+            else
+                toastr.error(data.message)
+        }
+    })
+})
