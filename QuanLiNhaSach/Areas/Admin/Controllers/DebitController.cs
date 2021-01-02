864ac0218b39729496c519aefc951843bef3f2b7
Phạm Hoàng <hoangcf7a2@gmail.com>
2021-01-02 12:25:01 +0700

    fix lỗi debit done 2

--- QuanLiNhaSach/Areas/Admin/Controllers/DebitController.cs
+++ QuanLiNhaSach/Areas/Admin/Controllers/DebitController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuanLiNhaSach.Data;
@@ -13,9 +14,11 @@ namespace QuanLiNhaSach.Areas.Admin.Controllers
     public class DebitController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public DebitController(ApplicationDbContext db)
+        private readonly UserManager<AppUser> _usermanager;
+        public DebitController(ApplicationDbContext db, UserManager<AppUser> usermanager)
         {
             _db = db;
+            _usermanager = usermanager;
         }
         public IActionResult Index()
         {
@@ -43,41 +46,65 @@ namespace QuanLiNhaSach.Areas.Admin.Controllers
             }
             return Json(new { data = obj });
         }
-        public IActionResult Update_List_Debit(string id) // hàm này sẽ tự động tạo debit detail từ các debit detail có của tháng trước
+        public IActionResult Create_List_Debit(string id) // hàm sẽ tạo debit detail từ new first debit, new incurred debit , new last debit của user
         {
-            DateTime timenow = DateTime.Parse(id);
-            var time_now = timenow.ToString("MM-yyyy");
-            var time_past = timenow.AddMonths(-1).ToString("MM-yyyy");
-            var list_debit_detail = _db.DebitDetails.ToList();
-            List<DebitDetail> list_debit_detail_last_month = new List<DebitDetail>();
-            DebitDetail debit_detail_has_time_now = null;
-            foreach (var item in list_debit_detail)
+            var timenow = DateTime.Parse(id); // biến string nhập vào là 1 ngày
+            var customerlist = _usermanager.GetUsersInRoleAsync("Customer").Result; // tạo 1 list customer role là khách hàng từ list user
+            DebitDetail debitdetail = new DebitDetail();
+            foreach (var item in customerlist)
             {
-                if (item.TimeRecord.ToString("MM-yyyy") == time_now)
+                if (item.new_last_debit != 0)
                 {
-                    debit_detail_has_time_now = item;
-                    break;
+                    debitdetail.Id = Guid.NewGuid().ToString();
+                    debitdetail.ApplicationUserId = item.Id;
+                    debitdetail.TimeRecord = timenow;
+                    debitdetail.FirstDebit = item.new_first_debit;
+                    debitdetail.IncurredDebit = item.new_incurred_debit;
+                    debitdetail.LastDebit = item.new_last_debit;
+                    _db.DebitDetails.Add(debitdetail);
+                    _db.SaveChanges();
+                    item.new_first_debit = item.new_last_debit;
+                    item.new_incurred_debit = 0;
+                    item.new_last_debit = item.new_first_debit + item.new_incurred_debit;
+                    _db.AppUsers.Update(item);
+                    _db.SaveChanges();
                 }
-                else if (item.TimeRecord.ToString("MM-yyyy") == time_past)
-                    list_debit_detail_last_month.Add(item);
             }
-            if (debit_detail_has_time_now != null) // nếu đã có debit detail của tháng hiện tại , thì  không thể tạo được nữa
-                return Json(new { success = false });
+            return Json(new { success = true}); 
             
-            DebitDetail newdebit = new DebitDetail();
-            foreach (var item in list_debit_detail_last_month)
-            {
-                newdebit.Id = Guid.NewGuid().ToString();
-                newdebit.ApplicationUserId = item.ApplicationUserId;
-                newdebit.TimeRecord = DateTime.Parse(time_now); // tháng hiện tại
-                newdebit.FirstDebit = item.LastDebit;
-                newdebit.IncurredDebit = 0;
-                newdebit.LastDebit = newdebit.FirstDebit + newdebit.IncurredDebit;
-                _db.DebitDetails.Add(newdebit);
-                _db.SaveChanges();
-            }
-            return Json(new { success = true });
         }
+        //DateTime timenow = DateTime.Parse(id);
+        //var time_now = timenow.ToString("MM-yyyy");
+        //var time_past = timenow.AddMonths(-1).ToString("MM-yyyy");
+        //var list_debit_detail = _db.DebitDetails.ToList();
+        //List<DebitDetail> list_debit_detail_last_month = new List<DebitDetail>();
+        //DebitDetail debit_detail_has_time_now = null;
+        //foreach (var item in list_debit_detail)
+        //{
+        //    if (item.TimeRecord.ToString("MM-yyyy") == time_now)
+        //    {
+        //        debit_detail_has_time_now = item;
+        //        break;
+        //    }
+        //    else if (item.TimeRecord.ToString("MM-yyyy") == time_past)
+        //        list_debit_detail_last_month.Add(item);
+        //}
+        //if (debit_detail_has_time_now != null) // nếu đã có debit detail của tháng hiện tại , thì  không thể tạo được nữa
+        //    return Json(new { success = false });
+
+        //DebitDetail newdebit = new DebitDetail();
+        //foreach (var item in list_debit_detail_last_month)
+        //{
+        //    newdebit.Id = Guid.NewGuid().ToString();
+        //    newdebit.ApplicationUserId = item.ApplicationUserId;
+        //    newdebit.TimeRecord = DateTime.Parse(time_now); // tháng hiện tại
+        //    newdebit.FirstDebit = item.LastDebit;
+        //    newdebit.IncurredDebit = 0;
+        //    newdebit.LastDebit = newdebit.FirstDebit + newdebit.IncurredDebit;
+        //    _db.DebitDetails.Add(newdebit);
+        //    _db.SaveChanges();
+        //}
+        //return Json(new { success = true });
     }
-     
+
 }
--- QuanLiNhaSach/wwwroot/Admin/js/debit.js
+++ QuanLiNhaSach/wwwroot/Admin/js/debit.js
@@ -48,11 +48,11 @@ $('#button_update_debit').on('click', function () {
     var time = $('#time_debit').val();
     console.log(time)
     $.ajax({
-        url: "/admin/debit/Update_List_Debit/" + time,
+        url: "/admin/debit/Create_List_Debit/" + time,
         success: function (data) {
             if (data.success) {
                 swalWithBootstrapButtons.fire(
-                    'Đã cập nhật thành công',
+                    'Đã tạo báo cáo thành công',
                     data.message,
                     'success'
                 );
@@ -60,7 +60,7 @@ $('#button_update_debit').on('click', function () {
             }
             else {
                 swalWithBootstrapButtons.fire(
-                    'Đã update nợ rồi, không thể thực hiện lại',
+                    'Không thể tạo báo cáo , vui lòng kiểm tra lại',
                     data.message,
                     'error'
                 )
