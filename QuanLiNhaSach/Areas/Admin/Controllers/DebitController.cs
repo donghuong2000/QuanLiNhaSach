66e8f618cf05ade67703689b3feab9dde4dd0c11
Phạm Hoàng <hoangcf7a2@gmail.com>
2021-01-03 14:17:53 +0700

    fix lỗi khi trong list debit detail không còn gì mà update lại không được

--- QuanLiNhaSach/Areas/Admin/Controllers/DebitController.cs
+++ QuanLiNhaSach/Areas/Admin/Controllers/DebitController.cs
@@ -103,6 +103,7 @@ namespace QuanLiNhaSach.Areas.Admin.Controllers
                 return Json(new { success = false, message = "Chưa tạo báo cáo của tháng hiện tại, nên không thể cập nhật báo cáo được" });
             var customerlist = _usermanager.GetUsersInRoleAsync("Customer").Result; // tạo 1 list customer role là khách hàng từ list user
             DebitDetail debitdetail = new DebitDetail();
+            Remove_debit_detail_in_month(timenow);
             foreach (var item in customerlist)
             {
                 if (item.old_last_debit != 0 ) // nếu đã tồn tại nợ cũ
@@ -112,7 +113,7 @@ namespace QuanLiNhaSach.Areas.Admin.Controllers
                     item.old_last_debit = item.old_first_debit + item.old_incurred_debit;
                     _db.AppUsers.Update(item);
                     _db.SaveChanges();
-                    Remove_debit_detail_in_month(timenow);
+
                     // xóa các debit detail cũ của tháng đó đi
                     // add lại debit detail cho user với incurred mới
                     debitdetail.Id = Guid.NewGuid().ToString();
